6d01463fe1c62e0574b09a32a644c88be6032bc1
minu2k5 <77044956+minhmap123@users.noreply.github.com>
2024-11-11 22:21:55 +0700

    Create SoPhuc.h

---
+++ Lab3-BTTL/Bai2/SoPhuc.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+using namespace std;
+
+/**
+ * @class SoPhuc
+ * @brief Lớp biểu diễn số phức với phần thực và phần ảo.
+ */
+class SoPhuc {
+private: 
+    float iThuc;    // Phần thực của số phức
+    float iAo;      // Phần ảo của số phức
+
+public: 
+    SoPhuc();
+    SoPhuc(float thuc, float ao);
+
+    friend SoPhuc operator + (const SoPhuc& a, const SoPhuc& b);
+    friend SoPhuc operator - (const SoPhuc& a, const SoPhuc& b);
+    friend SoPhuc operator * (const SoPhuc& a, const SoPhuc& b);
+    friend SoPhuc operator / (const SoPhuc& a, const SoPhuc& b);
+
+    friend bool operator == (const SoPhuc& a, const SoPhuc& b);
+    friend bool operator != (const SoPhuc& a, const SoPhuc& b);
+
+    friend ostream& operator << (ostream &os, const SoPhuc& ps);
+    friend istream& operator >> (istream &is, SoPhuc& ps);
+};
